eaafa17f6a461192bd00dfb041c68d2f965448df
wanggugu <87158048+wanggugu197@users.noreply.github.com>
2025-10-19 23:36:16 +0800

    修改gradle

--- src/main/java/org/com/gtoeep/GTOEpp.java
+++
@@ -1,8 +0,0 @@
-package org.com.gtoeep;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(GTOEpp.MODID)
-public class GTOEpp {
-    public static final String MODID = "gtoepp";
-}
---
+++ src/main/java/org/com/gtoepp/GTOEpp.java
@@ -0,0 +1,9 @@
+package org.com.gtoepp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GTOEpp.MODID)
+public class GTOEpp {
+
+    public static final String MODID = "gtoepp";
+}
